6166a3bacf0af79ab80369d9b35e7b720bd8b294
paul <paul@mpb-wifi.thepalace.darktech.org>
2018-02-11 13:28:31 -0500

    Initial commit

---
+++ Marlin/make_tags.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+CURRENT_DIR=`pwd`/
+rm tags cscope* 2> /dev/null
+echo ----- Building Tags -----
+ctags -R $CURRENT_DIR
+
+echo ----- Building cscope -----
+find $CURRENT_DIR -iname "*.[ch]" | awk '{print "\""$0"\""}' > cscope.in
+find $CURRENT_DIR -iname "*.cc" | awk '{print "\""$0"\""}' >> cscope.in
+cscope -b -k -q -u -i cscope.in
+
+echo ----- Done -----
